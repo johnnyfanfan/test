b5208ef9ba339ab6019ee97bd65d2e869c86cd27
johnnyfanfan <johnny_fanjinxiang@outlook.com>
2016-08-26 10:54:31 +0800

    all

--- webapps/johnny/WEB-INF/classes/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE if not exists contacts(
-	id int primary key auto_increment,
-	firstName varchar(30) not null,
-	lastName varchar(50) not null,
-	phoneNumber varchar(13),
-	emailAddress varchar(30)
-);
